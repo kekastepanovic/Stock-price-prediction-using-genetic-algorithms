e87a4836b1733c883a9b9130f365818b54302d32
kekastepanovic <45943881+kekastepanovic@users.noreply.github.com>
2019-12-02 19:54:42 +0100

    RouletteWheel algorithm based on chromosome's fitness function

---
+++ RouletteWheel.m
@@ -0,0 +1,24 @@
+function index=RouletteWheel(arrayInput) 
+len=length(arrayInput);
+if(len==1)
+    index=1;
+end
+if (~isempty(find(arrayInput<1, 1)))
+    if (min(arrayInput) ~=0)
+    arrayInput = 1/min(arrayInput)*arrayInput;
+    else
+    temp= arrayInput;
+    temp(arrayInput==0) = inf;
+    arrayInput = 1/min(temp)*arrayInput;
+    end
+end
+
+
+temp=0;
+tempProb=zeros(1,len);
+for i=1:len
+    tempProb(i)=temp+arrayInput(i);
+    temp=tempProb(i);
+end
+i=fix(rand*floor(tempProb(end)))+1;
+index=find(tempProb>=i,1);
